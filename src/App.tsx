462a197e3a45e74f70aa970a26d16791b98ac484
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2024-02-13 16:07:09 +0200

    added requestswith server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+/* eslint-disable */
+
 import React from 'react';
 import { InputForm } from './components/InputForm';
 import './App.scss';
 
 export const App: React.FC = () => {
-  const handleStart = (limit: number) => {
-    // eslint-disable-next-line no-console
+  const handleClickStart = async (limit: number) => {
+    try {
+      const requests = Array.from({ length: limit }, (_, index) => index);
+
+      const responses = await Promise.all(requests.map(async (index) => {
+        const response = await fetch(`http://localhost:5000/api?index=${index}&limit=${limit}`, {
+          method: 'GET',
+        });
+        return response.text();
+      }));
+
+      responses.forEach((data, index) => {
+        console.log(`Response from server ${index}:`, data);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleStart = async (limit: number) => {
     console.log('Starting with limit:', limit);
+    await handleClickStart(limit);
   };
 
   return (
