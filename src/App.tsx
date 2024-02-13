a3d7ca15b0ba118d104926233641ef2ca0da463a
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2024-02-13 20:29:35 +0200

    refactored code and fixed bugs logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,59 @@
 /* eslint-disable */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { InputForm } from './components/InputForm';
 import './App.scss';
 import _ from 'lodash';
 
 export const App: React.FC = () => {
   const [sizeChunks, setSizeChunks] = React.useState<number>(10);
-
-  const handleClickStart = async (limit: number) => {
+  const [list, setList] = React.useState<string[]>([]);
+ 
+  const handleClickStart = async () => {
     try {
-      const requests = Array.from({ length: limit }, (_, index) => index + 1);
+      const requests = Array.from(Array(100).keys());
 
       const allPromises = requests.map(async (index) => {
-        const response = await fetch(`http://localhost:5000/api?index=${index}&limit=${limit}`, {
-          method: 'GET',
-        });
+        const response = await fetch(
+          `http://localhost:5000/api?index=${index}&limit=${sizeChunks}`,
+          {
+            method: "GET",
+          }
+        );
         return response.text();
-      })
-
+      });
       const promisesChunks = _.chunk(allPromises, sizeChunks);
-
       promisesChunks.forEach(async (chunk) => {
         const responses = await Promise.all(chunk);
-
-        responses.forEach((data, index) => {
-          console.log(`Response from server ${index}:`, data);
-        });
-      })
+        setList((prev) => [...prev, ...responses]);
+      });
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
-  const handleStart = async (limit: number) => {
-    console.log('Starting with limit:', limit);
-    await handleClickStart(limit);
+  type ItemProps = {
+    index: any;
   };
+  const Item: React.FC<ItemProps> = ({ index }) => <p>{index}</p>;
+
+  const concurrencyLimit = "100";
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Client-Server Form</h1>
-      <InputForm onStart={handleStart} formOnChange={setSizeChunks} />
+     
+      <InputForm
+        onStart={handleClickStart}
+        formOnChange={setSizeChunks}
+        concurrencyLimit={concurrencyLimit}
+      />
+
+      <div>
+        {list.map((i, index) => (
+          <Item index={i} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -3,42 +3,40 @@ import React, { useState } from 'react';
 import './InputForm.scss';
 
 interface InputFormProps {
-  onStart: (limit: number) => void;
+  onStart: () => void;
   formOnChange: (value: number) => void;
+  concurrencyLimit: string
 }
 
-export const InputForm: React.FC<InputFormProps> = ({ onStart, formOnChange }) => {
-  const [inputValue, setInputValue] = useState(0);
-  const [error, setError] = useState<string | null>(null);
+export const InputForm: React.FC<InputFormProps> = ({
+  formOnChange,
+  onStart,
+  concurrencyLimit,
+}) => {
   const [submitted, setSubmitted] = useState<boolean>(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (Number.isNaN(inputValue) || inputValue < 0 || inputValue > 100) {
-      setError('Please enter a number between 0 and 100');
-    } else {
-      onStart(inputValue);
-      setInputValue(0);
-      setSubmitted(true);
-    }
+    onStart();
+    setSubmitted(true);
   };
 
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
         <input
-          type="text"
-          value={inputValue}
+          type="number"
+          max={concurrencyLimit}
           onChange={(e) => {
-            setInputValue(Number(e.target.value));
             formOnChange(Number(e.target.value));
           }}
           placeholder="Enter a number between 0 and 100"
         />
         <button type="submit">Start</button>
-        {error && <div className="error">{error}</div>}
-        {submitted && !error && <div className="success">Form submitted successfully</div>}
+
+        {submitted && (
+          <div className="success">Form submitted successfully</div>
+        )}
       </form>
     </div>
   );
