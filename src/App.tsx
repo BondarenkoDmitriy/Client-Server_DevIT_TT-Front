343b1606f20903c48491d323c80c6e65d82f4495
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2024-02-13 18:08:33 +0200

    added chunk logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@
 import React from 'react';
 import { InputForm } from './components/InputForm';
 import './App.scss';
+import _ from 'lodash';
 
 export const App: React.FC = () => {
   const handleClickStart = async (limit: number) => {
     try {
-      const requests = Array.from({ length: limit }, (_, index) => index);
+      const requests = Array.from({ length: limit }, (_, index) => index + 1);
 
-      const responses = await Promise.all(requests.map(async (index) => {
+      const allPromises = requests.map(async (index) => {
         const response = await fetch(`http://localhost:5000/api?index=${index}&limit=${limit}`, {
           method: 'GET',
         });
         return response.text();
-      }));
+      })
 
-      responses.forEach((data, index) => {
-        console.log(`Response from server ${index}:`, data);
-      });
+      const promisesChunks = _.chunk(allPromises, 10);
+
+      promisesChunks.forEach(async (chunk) => {
+        const responses = await Promise.all(chunk);
+
+        responses.forEach((data, index) => {
+          console.log(`Response from server ${index}:`, data);
+        });
+      })
     } catch (error) {
       console.error('Error:', error);
     }
